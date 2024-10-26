3ae534ac2467346349233284eb7fdfb838a0ee31
손명진 <151160972+SonMyeongJin@users.noreply.github.com>
2024-10-26 19:01:45 +0900

    [Bronze V] Title: 개, Time: 168 ms, Memory: 17556 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
