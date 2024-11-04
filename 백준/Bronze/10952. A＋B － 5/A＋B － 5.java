406edae7a82ff764d72eedddfc5afb9e9c1c470b
손명진 <151160972+SonMyeongJin@users.noreply.github.com>
2024-11-04 22:42:28 +0900

    [Bronze V] Title: A+B - 5, Time: 116 ms, Memory: 14596 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(line);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            if (a == 0 && b == 0) break;
+
+            bw.write((a + b) + "\n");
+        }
+
+        bw.flush();
+    }
+}
