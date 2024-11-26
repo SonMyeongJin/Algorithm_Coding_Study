a41805462700b82651d5146734942006ccd6c12d
손명진 <151160972+SonMyeongJin@users.noreply.github.com>
2024-11-26 23:30:31 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 184 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 단어 입력받기
+        String word = sc.nextLine();
+
+        // 단어 길이 출력
+        System.out.println(word.length());
+    }
+}
